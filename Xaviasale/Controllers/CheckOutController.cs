25d15604d36207102aae98a3c5f317d85dd0d382
nhatstrongtie <nhatnguyen@strongtie.com>
2022-09-29 01:35:37 +0700

    Update url callback API

--- Xaviasale/Controllers/CheckOutController.cs
+++ Xaviasale/Controllers/CheckOutController.cs
@@ -37,7 +37,9 @@ namespace Xaviasale.Controllers
                 }, JsonRequestBehavior.AllowGet);
             }
             var home = Umbraco.Content(model.Carts.FirstOrDefault().ProductId).Root();
-            var returnData = Checkout(model.Carts, model.PaymentMethod, home.DescendantOfType("checkOutSuccessPage")?.Url(mode: UrlMode.Absolute), model);
+            var successUrl = home.DescendantOfType("checkOutSuccessPage")?.Url(mode: UrlMode.Absolute);
+            var notifyUrl = home.Url(mode: UrlMode.Absolute).Substring(0, home.Url(mode: UrlMode.Absolute).IndexOf('/', "https://".Length)) + "/umbraco/surface/checkout/PaymentNotify";
+            var returnData = Checkout(model.Carts, model.PaymentMethod, successUrl, notifyUrl, model);
             var result = JsonConvert.DeserializeObject<CoinHomePayOrderReturnModel>(returnData);
             Session[AppConstant.SESSION_CART_ITEMS] = null;
             return Json(new
@@ -47,7 +49,7 @@ namespace Xaviasale.Controllers
                 data = result
             }, JsonRequestBehavior.AllowGet);
         }
-        private string Checkout(List<Cart> model, string paymentMethod, string redirectUrl, CheckOutModel checkoutModel)
+        private string Checkout(List<Cart> model, string paymentMethod, string redirectUrl, string notifyUrl, CheckOutModel checkoutModel)
         {
             var resGuid = SaveOrdersToDatabase(checkoutModel);
             var merchanId = WebConfigurationManager.AppSettings["CoinHomePay.MerchanId"];
@@ -92,7 +94,8 @@ namespace Xaviasale.Controllers
                 }
             }
             redirectUrl = redirectUrl + "?data=" + resGuid;
-            var signString = $"channel={paymentMethod}&goodsName={goodName}&merchantId={merchanId}&money={money}&notifyUrl={redirectUrl}&outBody={outBody}&outTradeNo={outTradeNo}&returnUrl={redirectUrl}";
+            notifyUrl = notifyUrl + "?data=" + resGuid;
+            var signString = $"channel={paymentMethod}&goodsName={goodName}&merchantId={merchanId}&money={money}&notifyUrl={notifyUrl}&outBody={outBody}&outTradeNo={outTradeNo}&returnUrl={redirectUrl}";
             if (sweepFeeUser > 0)
             {
                 signString += "&sweepFeeUser=" + sweepFeeUser;
@@ -108,7 +111,7 @@ namespace Xaviasale.Controllers
                 new KeyValuePair<string, string>("money", money.ToString()),
                 new KeyValuePair<string, string>("channel", paymentMethod),
                 new KeyValuePair<string, string>("outTradeNo", outTradeNo),
-                new KeyValuePair<string, string>("notifyUrl", redirectUrl),
+                new KeyValuePair<string, string>("notifyUrl", notifyUrl),
                 new KeyValuePair<string, string>("returnUrl", redirectUrl),
                 new KeyValuePair<string, string>("goodsName", goodName),
                 new KeyValuePair<string, string>("outBody", outBody)
@@ -211,6 +214,128 @@ namespace Xaviasale.Controllers
                 vResult.View.Render(vContext, writer);
                 return writer.ToString();
             }
+        }
+        [HttpGet]
+        public void PaymentNotify(string data, bool status)
+        {
+            if (!string.IsNullOrEmpty(data))
+            {
+                using (var db = new XaviasaleContext())
+                {
+                    var param = data.ToLower();
+                    var order = db.Orders.FirstOrDefault(x => x.ResponGuid.ToString().ToLower().Equals(param));
+                    if (order != null)
+                    {
+                        if (status)
+                        {
+                            order.Status = true;
+                            order.IsSuccess = true;
+                        }
+                        db.Orders.Attach(order);
+                        db.Entry(order).Property(x => x.IsSuccess).IsModified = true;
+                        db.Entry(order).Property(x => x.Status).IsModified = true;
+                        db.SaveChanges();
+                        var obj = new CheckOutModel
+                        {
+                            Email = order.Email,
+                            FirstName = order.FirstName,
+                            LastName = order.LastName,
+                            Address = order.Address,
+                            Apartment = order.Apartment,
+                            ZipCode = order.ZipCode,
+                            City = order.City,
+                            State = order.State,
+                            Country = order.Country,
+                            Phone = order.Phone
+                        };
+                        obj.Carts = db.ShoppingCarts.Where(x => x.OrderId == order.OrderId)
+                            .Select(x => new Cart
+                            {
+                                Color = x.Color,
+                                CouponId = x.CouponId,
+                                ProductId = x.ProductId,
+                                Quantity = x.Quantity
+                            }).ToList();
+                        var email = RenderMailMessage(obj);
+                        try
+                        {
+                            var smtp = new SmtpClient();
+                            smtp.Send(email);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+        private MailMessage RenderMailMessage(CheckOutModel model)
+        {
+            decimal total = 0;
+
+            var sendTo = WebConfigurationManager.AppSettings["EmailContactReceive"];
+            var messageString = "<h3>" + WebConfigurationManager.AppSettings["EmailCheckOutTitle"] + "</h3>";
+            messageString += "<b>Email: </b>" + model.Email + "<br />";
+            messageString += "<b>First Name: </b>" + model.FirstName + "<br />";
+            messageString += "<b>Last Name: </b>" + model.LastName + "<br />";
+            messageString += "<b>Address: </b>" + model.Address + "<br />";
+            messageString += "<b>Apartment, suite, etc.: </b>" + model.Apartment + "<br />";
+            messageString += "<b>Zip Code: </b>" + model.ZipCode + "<br />";
+            messageString += "<b>City: </b>" + model.City + "<br />";
+            messageString += "<b>State: </b>" + model.State + "<br />";
+            messageString += "<b>Country: </b>" + model.Country + "<br />";
+            messageString += "<b>Phone number: </b>" + model.Phone + "<br />";
+            messageString += "<p>==================================================</p>";
+            messageString += "<b>Orders: </b><br />";
+            messageString += "<ul>";
+            if (model.Carts != null)
+            {
+                var hasCoupon = false;
+                foreach (var item in model.Carts)
+                {
+                    decimal discount = 0;
+                    var couponName = string.Empty;
+                    if (item.CouponId > 0 && hasCoupon == false)
+                    {
+                        var coupon = Umbraco.Content(item.CouponId);
+                        discount = coupon.Value<decimal>("discount");
+                        couponName = coupon.Name;
+                    }
+                    var product = Umbraco.Content(item.ProductId);
+                    if (product != null)
+                    {
+                        var itemColorNested = product.Value<IEnumerable<IPublishedElement>>("productColorNested").FirstOrDefault(x => x.Value<string>("title").Equals(item.Color));
+                        if (itemColorNested != null)
+                        {
+                            messageString += "<li>";
+                            messageString += "<b>Sản phẩm: </b> <a href='" + product.Url(mode: UrlMode.Absolute) + "'>" + product.Name + "</a><br />";
+                            messageString += "<b>Color: </b>" + item.Color + "<br />";
+                            messageString += "<b>Số lượng: </b> " + item.Quantity + "<br />";
+                            if (!string.IsNullOrEmpty(couponName))
+                            {
+                                messageString += "<b>Coupon: </b> " + couponName + "<br />";
+                            }
+                            messageString += "<b>Giá (đơn vị 1 cái): </b> $" + (discount > 0 ? itemColorNested.Value<decimal>("price") - (itemColorNested.Value<decimal>("price") * (discount / 100)) : itemColorNested.Value<decimal>("price")) + "<br />";
+                            messageString += "<b>Tổng: </b> $" + (discount > 0 ? (itemColorNested.Value<decimal>("price") - (itemColorNested.Value<decimal>("price") * (discount / 100))) * item.Quantity : itemColorNested.Value<decimal>("price") * item.Quantity) + "<br />";
+                            messageString += "</li>";
+                            total += (discount > 0 ? (itemColorNested.Value<decimal>("price") - (itemColorNested.Value<decimal>("price") * (discount / 100))) * item.Quantity : itemColorNested.Value<decimal>("price") * item.Quantity);
+                        }
+                    }
+                }
+            }
+            messageString += "</ul>";
+            messageString += "<p>==================================================</p>";
+            messageString += "<b>Tổng tiền: </b>$" + total + "<br />";
+
+            var email = new MailMessage
+            {
+                Subject = "Order",
+                Body = messageString,
+                IsBodyHtml = true,
+                To = { sendTo }
+            };
+            return email;
         }
     }
 }

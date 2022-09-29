dc19f93057e785a241de843660da85fb851c6156
nhatstrongtie <nhatnguyen@strongtie.com>
2022-09-29 15:41:46 +0700

    thêm update date

---
+++ Xaviasale.EntityFramework/Migrations/202209290829579_AddColumnOrderUpdateDate.cs
@@ -0,0 +1,18 @@
+﻿namespace Xaviasale.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddColumnOrderUpdateDate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Orders", "UpdateDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Orders", "UpdateDate");
+        }
+    }
+}
